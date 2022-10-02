33bf6263be4c56887899f02a81bb23f91cffe626
behemoth-truongnd <truongnd@behemoth.vn>
2022-10-02 16:10:27 +0700

    add validate for youtube video

--- app/models/youtube_video.rb
+++ app/models/youtube_video.rb
@@ -1,3 +1,19 @@
 class YoutubeVideo < ApplicationRecord
   belongs_to :user
+
+  validates :youtube_id, :title, presence: true
+  validates :youtube_id, length: { maximum: 255 }
+
+  validate :validate_youtube_id
+
+  private
+
+  def validate_youtube_id
+    return if youtube_id.blank?
+
+    yt = Yt::Video.new(id: youtube_id)
+    yt.title
+  rescue
+    errors.add(:youtube_id, :invalid)
+  end
 end
